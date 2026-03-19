7c21de9582cb1adb9b0fa3cb59852491cf0b65fb
Rohit Pokala <rohitpokala22@gmail.com>
2026-03-19 21:29:28 +0530

    Dev Testing

--- user-service/src/main/java/com/devops/userservice/controller/UserController.java
+++ user-service/src/main/java/com/devops/userservice/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-
+// UserController.java
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
